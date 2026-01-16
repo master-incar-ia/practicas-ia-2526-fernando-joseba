24a3bc2a75615069af124f2222e2d3382ae27fe3
Joseba1410 <frodriguez031@ikasle.ehu.eus>
2026-01-16 17:57:54 +0100

    Model correcto

--- src/exercise_01/model.py
+++ src/exercise_01/model.py
@@ -3,6 +3,19 @@ import torch.nn as nn
 
 
 # Debe tener el init y el forward
+class SimplePerceptron(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self.fc = nn.Linear(input_dim, output_dim)
+        self.activation = nn.Identity()
+
+    def forward(self, x, use_activation=True):
+        x = self.fc(x)
+        if use_activation:
+            x = self.activation(x)
+        return x
+
+
 class MultiLayerPerceptron(nn.Module):  # Hereda de nn.Module, es un requisito
     def __init__(
         self, input_dim, output_dim, num_hidden_neurons, apodo
@@ -18,7 +31,7 @@ class MultiLayerPerceptron(nn.Module):  # Hereda de nn.Module, es un requisito
 
     def forward(self, x, use_activation=True):
         x1 = self.fc1(x)  # Falta la activacion
-        x1 = self.activation_relu(x1)  # Aplicar la activacion, relu porque es no lineal
+        x1 = self.activation(x1)  # Aplicar la activacion, relu porque es no lineal
         x2 = self.fc2(x1)
 
         if use_activation:
